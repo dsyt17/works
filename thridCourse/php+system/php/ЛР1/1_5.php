18b3f8e9902163e052fbf8edb4795a9b003127db
Your Name <goncharovigor12@gmail.com>
2022-08-27 13:24:05 +0300

    readmemain

--- thridCourse/php+system/php/ЛР1/1_5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$x = -2.34;
-
-$result = ((abs($x-5)-sin($x))/3) + (sqrt(pow($x, 2)+2014)) * cos($x*2) - 3;
-
-echo $result;
-
-?>
--- thridCourse/матметоды+анализданных/irisy_cluster/irisy_cluster.py
+++
@@ -1,9 +0,0 @@
-import csv
-import string
-
-with open('irisy.csv') as data:
-    for row in data.readlines():
-        row.replace(',', '.')
-    data = [list(map(int, row.split())) for row in data.readlines()]
-
-print(data)
