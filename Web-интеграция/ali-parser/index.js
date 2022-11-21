f71921ceeb39f3f3dc340f4bf0ac51407800de4d
Your Name <goncharovigor12@gmail.com>
2022-11-21 10:28:54 +0300

    sql_done

--- Web-интеграция/ali-parser/index.js
+++ Web-интеграция/ali-parser/index.js
@@ -1,9 +1,9 @@
 import cheerio from "cheerio";
 import fs from "fs";
+import mysql from "mysql2";
 
 const parse = () => {
-  const data = fs.readFileSync("./data2.txt", "utf8");
-  // console.log(data);
+  const data = fs.readFileSync("./data.txt", "utf8");
 
   let $ = cheerio.load(data);
   $("#productDescriptBox br").remove();
@@ -30,8 +30,55 @@ const parse = () => {
     available: $(".stock-num.mr5").text(),
     image: $(".max-photo").find("img").attr("src"),
   };
-
   console.log(item);
+
+  return item;
 };
 
-parse();
+const writeToMySql = (item) => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "admin",
+    database: "wordpress",
+    password: "225506",
+    socketPath: "/var/run/mysqld/mysqld.sock",
+  });
+};
+
+connection.connect((err) => {
+  if (err) {
+    return console.error("Ошибка: " + err.message);
+  }
+  else{
+    console.log("Подключение к серверу MySQL успешно установлено");
+  }
+
+  splittedData.forEach(e => {
+
+      var sql = "INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) "
+                +`VALUES (NULL, '1', '${new Date().toLocaleString()}', '${new Date().toLocaleString()}', '<!-- wp:paragraph -->\r\n<p><strong>Текущая цена:</strong> ${item.current_price}</p>\r\n<p><strong>Цена без скидки:</strong> ${item.full_price}</p>\r\n<p>Рейтинг: ${item.raiting} Из ${item.reviews} оценок</p>\r\n<p>Остаток: ${item.available} штук; В желаемом ${item.wish_list} оценок</p>\r\n<p>Доставка: ${item.shippment}</p>\r\n<img src="${item.image}"></img>\r\n<!-- /wp:paragraph -->', '${item.title}', '', 'publish', 'open', 'open', '', '${Math.floor(Math.random() * 100000000000000001)}', '', '', '2022-10-28 16:41:46', '2022-10-28 16:41:46', '', '0', 'http://goncharovigor.com/?p=1111', '0', 'post', '', '0'); `;
+  
+      connection.query(sql, function (err, result) {
+          if (err) {
+          return console.error("Ошибка: " + err.message);
+      }
+      console.log("Record inserted");
+      });
+
+  })
+
+
+  // закрытие подключения
+  // connection.end((err) => {
+      //     if (err) {
+          //         return console.log("Ошибка: " + err.message);
+          //     }
+          // console.log("Подключение закрыто");
+          // });
+
+
+});
+
+
+const item = parse();
+writeToMySql(item)
--- Web-интеграция/ali-parser/phantom.js
+++ Web-интеграция/ali-parser/phantom.js
@@ -1,17 +1,16 @@
-var page = new WebPage();
-var fs = require("fs");
-var cheerio = require("cheerio");
+const page = new WebPage();
+const fs = require("fs");
+const system = require("system");
+const args = system.args;
+
+const url = args[1];
 
 page.onLoadFinished = function () {
-  console.log("page load finished");
-  fs.write("data2.txt", page.content, "w");
-
+  console.log("Page loadead");
+  fs.write("data.txt", page.content, "w");
   phantom.exit();
 };
 
-page.open(
-  "https://www.dx.com/p/bluetooth-50-42-audio-receiver-transmitter-3-in-1-35mm-35-aux-jack-usb-stereo-music-wireless-adapters-for-car-tv-mp3-pc-2734705.html#.Y3qC9XZBxD8",
-  function () {
-    page.evaluate(function () {});
-  }
-);
+page.open(url, function () {
+  page.evaluate(function () {});
+});
