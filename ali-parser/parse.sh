cdd159d8d7f45cde2b01ff2bda3466eeecad6cc0
Igor Goncharov <goncharovigor12@gmail.com>
2022-11-22 14:26:54 +0300

    parsing

---
+++ ali-parser/parse.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+phantomjs phantom.js $1
+node index.js 
---
+++ ali-parser/phantom.js
@@ -0,0 +1,16 @@
+const page = new WebPage();
+const fs = require("fs");
+const system = require("system");
+
+const url = system.args[1];
+
+page.onLoadFinished = function () {
+    console.log(url);
+    console.log("Page loadead");
+    fs.write("responce_html.txt", page.content, "w");
+    phantom.exit();
+};
+
+page.open(url, function () {
+  page.evaluate(function () {});
+});
